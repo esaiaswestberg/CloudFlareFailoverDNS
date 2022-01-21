a48b103f762003848564caf46c69590fd4a5b555
Esaias Tilly <esaias@tillys.se>
2022-01-21 17:07:30 +0100

    Fetch public IP

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { get } from './utils/requestHandler';
+import { default as ipGetter} from 'public-ip';
 import { config } from 'dotenv';
 config();
 
-const main = async () => {
-	const resp = await get('https://jsonplaceholder.typicode.com/todos/1');
-	console.log(resp);
-};
-main();
+/**
+ * For master:
+ * 1. Get public IP.
+ * 2. Get current Cloudflare IP.
+ * 3. Check if local Bitwarden is running.
+ * 4. If running, change Cloudflare IP to public IP.
+ */
+
+// Fetch client public IP
+const publicIp = await ipGetter.v4();
+console.log(`Client IP: ${publicIp}`);
