b659d99f2a462faed1e8e1b6998bac7245360537
Esaias Tilly <esaias@tillys.se>
2022-01-21 19:23:30 +0100

    Added put

--- src/handlers/requestHandler.ts
+++ src/handlers/requestHandler.ts
@@ -8,10 +8,31 @@ import fetch from 'node-fetch';
  */
 export const get = async (url: string, headers: any) => {
 	// @ts-ignore
-    const response = await fetch(url, { headers });
-    if (response.ok) {
-        return await response.text();
-    }
+	const response = await fetch(url, { headers });
+	if (response.ok) {
+		return await response.text();
+	}
 
-    throw `Error on get request to ${url}:\n${response.status} ${response.statusText}`;
-}
+	throw `Error on get request to ${url}:\n${response.status} ${response.statusText}`;
+};
+
+/**
+ * Makes a PUT request to the given url and returns the response.
+ * @param url The url to PUT to.
+ * @param headers The headers to send.
+ * @param body The body to send.
+ * @returns The response string.
+ */
+export const put = async (url: string, headers: any, body: any) => {
+	const response = await fetch(url, {
+		headers,
+		method: 'PUT',
+		body
+	});
+
+	if (response.ok) {
+		return await response.text();
+	}
+
+	throw `Error on put request to ${url}:\n${response.status} ${response.statusText}`;
+};
