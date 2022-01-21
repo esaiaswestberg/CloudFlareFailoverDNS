321eacce357cfaf7f59a56fe9dc636e077deba77
Esaias Tilly <esaias@tillys.se>
2022-01-21 19:34:28 +0100

    Set cloudflare IP

--- src/handlers/cloudflareHandler.ts
+++ src/handlers/cloudflareHandler.ts
@@ -1,10 +1,5 @@
-import { get } from './requestHandler.js';
-import { config } from 'dotenv';
-config();
-
-const ZONE_ID = process.env.ZONE_ID;
-const RECORD_ID = process.env.RECORD_ID;
-const API_KEY = process.env.API_KEY;
+import { get, put } from './requestHandler.js';
+import { ZONE_ID, RECORD_ID, API_KEY, SERVICE_HOST } from '../env_constants.js';
 
 /**
  * Gets the current IP address of the Cloudflare DNS record.
@@ -20,4 +15,33 @@ export const getIp = async (): Promise<string> => {
 	);
 
 	return JSON.parse(resp).result.content;
+};
+
+/**
+ * Set the Cloudflare DNS record to the given IP.
+ * @param ip The IP address to set.
+ * @returns Whether the request was successful.
+ */
+export const setIp = async (ip: string): Promise<boolean> => {
+	try {
+		const resp = await put(
+			`https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${RECORD_ID}`,
+			{
+				'Authorization': `Bearer ${API_KEY}`,
+				'Content-Type': 'application/json'
+			},
+			{
+				'type': 'A',
+				'name': SERVICE_HOST,
+				'content': ip,
+				'ttl': 1,
+				'proxied': true
+			}
+		);
+
+		const data = JSON.parse(resp);
+		return !!data.success;
+	} catch {
+		return false;
+	}
 };
